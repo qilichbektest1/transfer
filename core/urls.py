fbe1c4a3df0ad43894723a8b8314f889f0826f8e
Пользователь <mansurovqilichbek39@gmail.com>
2025-11-21 17:54:01 +0500

    first commit

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import *
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',HomeView.as_view(), name = 'home'),
+    path('clubs/', ClubsView.as_view(),name = 'clubs'),
+    path('clubs/<int:pk>/details/', ClubDetailsView.as_view(),name = 'club-details'),
+    path('about/', AboutView.as_view(),name = 'about'),
 ]
 
 if settings.DEBUG:
--- main/views.py
+++ main/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.views import View
+from .models import Club
 
-# Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class ClubsView(View):
+    def get(self, request):
+        clubs = Club.objects.all()
+        context = {
+            'clubs': clubs
+        }
+        return render(request, 'clubs.html', context)
+
+
+class ClubDetailsView(View):
+    def get(self,request,pk):
+        club = get_object_or_404(Club,id=pk)
+        players = club.player_set.order_by('-price')
+        context = {
+            'club':club,
+            'players':players,
+        }
+        return render(request,'club-details.html',context)
+
+class AboutView(View):
+    def get(self, request):
+        return render(request,'about.html')
